88c6aacdfd587bbeb45d7394868ddcbaa5277e46
songah <ssongahlee@gmail.com>
2020-06-08 15:29:28 +0900

    jsp, thymeleaf 추가
    - thymeleaf는 안되고 있는데 원인 못찾음...

--- src/main/java/com/wonwoo/welcome/controller/SampleController.java
+++ src/main/java/com/wonwoo/welcome/controller/SampleController.java
@@ -1,8 +1,10 @@
 package com.wonwoo.welcome.controller;
 
 import com.wonwoo.welcome.service.SampleService;
+import com.wonwoo.welcome.vo.TestVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,38 +13,48 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+@RestController
 public class SampleController {
 
     @Autowired
     SampleService sampleService;
 
+//    @ResponseBody
     @RequestMapping(value = "/home")
     public String home() {
         return "index.html";
     }
 
-    @ResponseBody
+//    @ResponseBody
     @RequestMapping(value = "/valueTest")
     public String valueTest() {
         String value = "하이! 이제부터 시작!";
         return value;
     }
 
-    // jsp page가 있어야 동작하는 코드
+    // jsp page
 //    @ResponseBody
-//    @RequestMapping(value = "/test")
-//    public ModelAndView test() throws Exception {
-//        ModelAndView mav = new ModelAndView("test");
-//        mav.addObject("name", "ssongahlee");
-//
-//        List<String> testList = new ArrayList<String>();
-//        testList.add("a");
-//        testList.add("b");
-//        testList.add("c");
-//
-//        mav.addObject("list", testList);
-//        return mav;
-//    }
+    @RequestMapping("/test")
+    public ModelAndView test() throws Exception {
+        ModelAndView mav = new ModelAndView("test");
+        mav.addObject("name", "ssongahlee");
+
+        List<String> testList = new ArrayList<String>();
+        testList.add("a");
+        testList.add("b");
+        testList.add("c");
+
+        mav.addObject("list", testList);
+        return mav;
+    }
+
+    // 이거 지금 안되는데 왜 안되는지 못찾겠다. thymeleafTest.html 파일을 못찾아감...
+//    @ResponseBody
+    @RequestMapping(value = "/thymeleafTest")
+    public String thymeleafTest(Model model) {
+        TestVo testModel = new TestVo("ssongahLee", "쏭아리");
+        model.addAttribute("testModel", testModel);
+        return "thymeleaf/thymeleafTest";
+    }
 
 }
---
+++ src/main/java/com/wonwoo/welcome/vo/TestVo.java
@@ -0,0 +1,37 @@
+package com.wonwoo.welcome.vo;
+
+public class TestVo {
+    private Long mbrNo;
+    private String id;
+    private String name;
+
+    public TestVo(String id, String name) {
+        super();
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getMbrNo() {
+        return mbrNo;
+    }
+
+    public void setMbrNo(Long mbrNo) {
+        this.mbrNo = mbrNo;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
